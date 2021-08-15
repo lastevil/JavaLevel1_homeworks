e638fc0cffab1d91fcb6a59029a739b9fb495fd4
Lastik <tolikrus@bk.ru>
2021-08-15 23:02:33 +0500

    Lesson_4 need to do: prove bugs

--- src/test/java/ru/geekbrains/lesson4/Lesson4.java
+++ src/test/java/ru/geekbrains/lesson4/Lesson4.java
@@ -21,10 +21,12 @@ public class Lesson4 {
 
     public static Scanner scanner;
     public static Random random;
+    public static int[] max;
 
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
+        max = new int[SIZE * 2 + 2];
         counterPlayer = 0;
 
 
@@ -59,7 +61,6 @@ public class Lesson4 {
 
 
     }
-
 
     //создание поля размеров SIZExSIZE
     private static void initMap() {
@@ -98,57 +99,104 @@ public class Lesson4 {
         lastTurnX = x;
         lastTurnY = y;
         counterPlayer++;
+        addToArrayMas();
+        System.out.println(Arrays.toString(max));
     }
     //ход машины
     private static void turnAI() {
-        int x, y;
-        int j3 = SIZE - 1;
+        int x=0,y=0,f=0;
         random = new Random();
-        int[] max = new int[SIZE*2 + 2];
+        int m =arrayMax();
         do {
-            //для первого хода
-            //точка Х
-            //          if (counterPlayer<=1) {
-            if (lastTurnX == 0) {
-                x = Math.round(random.nextInt(2));
-            } else if (lastTurnX == 2) {
-                x = 1 + random.nextInt(lastTurnX);
-            } else
-                x = random.nextInt(SIZE);
-            //точка Y
-            if (lastTurnY == 0) {
-                y = Math.round(random.nextInt(2));
-            } else if (lastTurnY == 2) {
-                y = 1 + random.nextInt(lastTurnY);
-            } else
-                y = random.nextInt(SIZE);
-            //  }
-            //         else {
-            //считаем колличество Х по строкам и столбцам с диагоналями
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (map[i][j] == DOT_X) {
-                        max[i] += 1;
-                    }
-                    if (map[j][i] == DOT_X) {
-                        max[i+SIZE] += 1;
-                    }
+            if(counterPlayer<1){
+                if (lastTurnX == 0) {//ход AI при количестве ходов игрока <2
+                    x = Math.round(random.nextInt(2));
                 }
-
-                if (map[i][i] == DOT_X) {
-                    max[max.length - 2] += 1;
+                else if (lastTurnX == 2) {
+                    x = 1 + random.nextInt(lastTurnX);
                 }
-                if (map[i][j3] == DOT_X) {
-                    max[max.length - 1] += 1;
-                    j3--;
+                else{
+                    x = random.nextInt(SIZE);
+                }
+                if (lastTurnY == 0) {
+                    y = Math.round(random.nextInt(2));
+                }
+                else if (lastTurnY == 2) {
+                    y = 1 + random.nextInt(lastTurnY);
+                }
+                else{
+                    y = random.nextInt(SIZE);
                 }
             }
-        System.out.println(Arrays.toString(max));
+            else{
+                if (m<((max.length/2)-2)){ //добавляем O в строку
+                    for (int i = 0; i <SIZE -1; i++) {
+                        if(map[m][i]==DOT_EMPTY && map[m][i+1]==DOT_X){
+                            y=i;
+                            x=m;
+                        } else if(map[m][i]==DOT_X && map[m][i+1]==DOT_EMPTY){
+                            y=i+1;
+                            x=m;
+                        }
 
-        } while (!checkTurnAI(x, y));
-        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
-        map[x][y] = DOT_O;
+                        else if(map[m][i]!=DOT_EMPTY&&map[m][i+1]!=DOT_EMPTY){
+                            x = random.nextInt(SIZE);
+                            y = random.nextInt(SIZE);
+                        }
+                    }
+                }
+                if ((m>=((max.length/2)-2)) && (m<((max.length)-2))){ //добавляем в столбец
+                    for (int i = 0; i <SIZE -1; i++) {
+                        if(map[i][m-SIZE]==DOT_EMPTY && map[i+1][m-SIZE]==DOT_X){
+                            x=i;
+                            y=m-(SIZE);
+                        } else if(map[i][m-SIZE]==DOT_X && map[i+1][m-SIZE]==DOT_EMPTY){
+                            x=i+1;
+                            y=m-SIZE;
+                        }
+                        else if(map[i][m-SIZE]!=DOT_EMPTY&&map[m][i+1]!=DOT_EMPTY){
+                            x = random.nextInt(SIZE);
+                            y = random.nextInt(SIZE);
+                        }
+                    }
+                }
+                if (m==(max.length-2)){ //добавляем в диагональ
+                    for (int i = 0; i <SIZE -1; i++) {
+                        if(map[i][i]==DOT_EMPTY && map[i+1][i+1]==DOT_X){
+                            x=i;
+                            y=i;
+                        } else if(map[i][i]==DOT_X && map[i+1][i+1]==DOT_EMPTY){
+                            x=i+1;
+                            y=i+1;
+                        }
+                        else if(max[m]>DOTS_TO_WIN-1){
+                            x = random.nextInt(SIZE);
+                            y = random.nextInt(SIZE);
+                        }
+                    }
+                }
+                else{ //добавляем в вторую диагональ
+                    for (int i = 0; i>SIZE-1; i--) {
+                        if(map[f][i]==DOT_EMPTY && map[f+1][i-1]==DOT_X){
+                            x=f;
+                            y=i;
+                        } else if(map[f][i]==DOT_X && map[f+1][i-1]==DOT_EMPTY){
+                            x=f+1;
+                            y=i-1;
+                        }
+                        else if(max[m]>DOTS_TO_WIN-1){
+                            x = random.nextInt(SIZE);
+                            y = random.nextInt(SIZE);
+                        }
+                        f++;
+                    }
+                }
+            }
 
+        } while(!checkTurnAI(x, y));
+    System.out.println("Компьютер походил в точку "+(x +1)+" "+(y +1));
+    map[x][y]=DOT_O;
+    clearArrayMas();
     }
     //проверка хода
     private static boolean checkTurn(int x, int y) {
@@ -179,14 +227,15 @@ public class Lesson4 {
         int checkDiag1 = 0;
         int checkDiag2 = 0;
         int y = SIZE - 1;
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {                //Идем по элементам массива
             for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == symb) {
+                if (map[i][j] == symb) {                //если найден массив с необходимым знаком
                     //проверка столбца
-                    for (int k = 0; k < SIZE; k++) {
+                    for (int k = 0; k < SIZE; k++) {    //проверяем весь столбик на наличие этого знака
                         if (map[i][k] == symb) {
                             checkColumn++;
-                            if (checkColumn == DOTS_TO_WIN) {
+                            if (checkColumn == DOTS_TO_WIN) { // если колличество элементов
+                                                                // совпало с количеством необходимым для победы
                                 return true;
                             }
                         } else checkColumn = 0;
@@ -205,12 +254,15 @@ public class Lesson4 {
                 }
             }
             //проверка диагоналей
-            if (map[i][i] == symb)
+            if (map[i][i] == symb){
                 checkDiag1++;
+            }
+            else if (map[i][i]!=symb){
+                checkDiag1--;
+            }
             if (checkDiag1 == DOTS_TO_WIN) {
                 return true;
             }
-
 
             if (map[i][y] == symb) {
                 checkDiag2++;
@@ -243,5 +295,66 @@ public class Lesson4 {
         return true;
 
     }
+    //наполнение массива весов(вычисляем после хода игрока)
+    public static void addToArrayMas(){
+        int j3 = SIZE - 1;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_X) {
+                    max[i] ++;
+                } else if(map[i][j]==DOT_O){
+                    max[i] --;
+                }
 
+                if (map[j][i] == DOT_X) {
+                    max[i+SIZE] ++;
+                }else if(map[j][i]== DOT_O){
+                    max[i+SIZE] --;
+                }
+            }
+            if (map[i][i] == DOT_X) {
+                max[max.length - 2] ++;
+            }else if(map[i][i] == DOT_O){
+                max[max.length - 2] --;
+            }
+            if (map[i][j3] == DOT_X) {
+                max[max.length - 1] ++;
+            }else if(map[i][j3] == DOT_O){
+                max[max.length - 1] --;
+            }
+            j3--;
+        }
+    }
+    //очистка массива весов(будем очищать после хода AI)
+    public static void clearArrayMas(){
+        for (int i = 0; i <max.length ; i++) {
+            max[i]=0;
+        }
+    }
+    //идекс максимального элемента мамассива весов
+    private static int arrayMax(){
+        int index1=0,index2=0,index=0;
+        int maximum = max[0];
+        for (int i = 1; i <max.length; i++) {
+            if(maximum<=max[i]){
+                maximum = max[i];
+                index1 = i;
+            }
+        }
+        for (int i = max.length-1; i >=0; i--) {
+            if(maximum<=max[i]){
+                maximum = max[i];
+                index2 = i;
+            }
+        }
+        if (index1==index2){
+        return index1;
+        }
+        else
+            if(Math.round(Math.random() * 7)>3){
+                index=index1;
+            }else
+                index=index2;
+            return index;
+    }
 }
